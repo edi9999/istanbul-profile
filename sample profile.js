3bc74cde5d8ed1df5c1e18508f6a6a230ff6ccd3
Ralph Varjabedian <ralph.varjabedian@rakutenloyalty.com>
2014-02-21 16:26:08 +0200

    fix

--- sample profile.js
+++ sample profile.js
@@ -1,86 +1,74 @@
 /**
- * Created by Ralph Varjabedian on 2/20/14.
- *
- * This is a sample profile.js file needed for the profiled code to run correctly
+ * Created by Ralph Varjabedian on 2/19/14.
  */
 
+var colors = require('colors');
 var fs = require("fs");
-var level = 0;
-var stack = [];
-var accumulate = {};
-var outputCsv = true;
 
-var outputFolder = "/lib-profile";
+var accumulate = {};
 
 function printAccumulate() {
    //json
-   fs.writeFileSync("profile_output_total.json", JSON.stringify(accumulate, null, 3));
+   fs.writeFileSync("profile_output_total.js", "var data = " + JSON.stringify(accumulate, null, 3));
    //csv
-   fs.writeFileSync("profile_output_total.csv", ['name','calls','total','subtotal','average','averagesub','\r\n'].join(','));
+   fs.writeFileSync("profile_output_total.csv", ['name', 'calls', 'total', 'average', '\r\n'].join(','));
    for (var k in accumulate) {
       fs.appendFileSync("profile_output_total.csv", [
-         '"' + k +'"',
+         '"' + k + '"',
          accumulate[k].calls,
          accumulate[k].total,
-         accumulate[k].subtotal,
          accumulate[k].total/accumulate[k].calls,
-         accumulate[k].subtotal/accumulate[k].calls,
          '\r\n'].join(','));
    }
 }
 
-setInterval(printAccumulate, 1000*10); // every 10 seconds
+setInterval(printAccumulate, 1000 * 10); // every 10 seconds
 
 module.exports = {
-   start: function(name, filename) {
-      console.log(this.getPrefix(level) + name + "() {", this.getFilename(filename));
-      stack.push({time: process.hrtime(), name: name, sub: 0});
-      level++;
-   },
-   end: function(name, filename) {
-      level--;
-      var call = stack.pop();
-      if (call.name != name)
-         throw new Error("invalid stack info");
-      var diff = process.hrtime(call.time);
-      var ms = (diff[0] * 1000) + (diff[1] / 1e6);
-      var parentCall = null;
-      if (stack.length > 0)
-         parentCall = stack[stack.length - 1];
-      if (parentCall)
-         parentCall.sub += ms;
-      if (call.sub) {
-         console.log(this.getPrefix(level) + "} ", ms, "ms", "sub:", call.sub, "diff:", ms - call.sub);
-         if (outputCsv) {
-            fs.appendFileSync("profile_output.csv", [
-               '"' + this.getFilename(filename) + '"', '"' + name + '"', ms, call.sub, ms - call.sub, "\r\n"].join(','));
-         }
-      }
-      else {
-         if (outputCsv) {
-            fs.appendFileSync("profile_output.csv", [
-               '"' + this.getFilename(filename) + '"', '"' + name + '"', ms, 0, 0, "\r\n"].join(','));
-         }
-         console.log(this.getPrefix(level) + "} ", ms, "ms");
-      }
+   start: function(name, filename, args) {
+      var _p = {name: name, filename: filename, cb: null};
 
-      var key = this.getFilename(filename) + ":" + name;
+      if (args.length > 0 && typeof(args[args.length - 1]) == "function") {
+         // callback mode
+         var self = this;
+         var originalcb = args[args.length - 1];
+         var override = function cb() { // how to create function signature similar to the old function, let's parse the arguments and generate one using eval?
+            self.endcb(_p);
+            return originalcb.apply(this, arguments);
+         }
+         _p.cb = override;
+         override._def = originalcb; // very important for DynamicHttpLayer
+      }
+      _p.time = process.hrtime();
+      return _p;
+   },
+   end: function(_p) {
+      if (_p.cb) {
+         // in finally and cb, ignore
+         return;
+      }
+      this.endcb(_p);
+   },
+   endcb: function(_p) {
+      // calculate time
+      var diff = process.hrtime(_p.time);
+      var ms = (diff[0] * 1000) + (diff[1] / 1e6);
+
+      // accumulate
+      var key = this.getFilename(_p.filename) + ":" + _p.name;
       if (!accumulate[key])
-         accumulate[key] = {calls:0,total:0, subtotal:0};
+         accumulate[key] = {calls: 0, total: 0};
       accumulate[key].calls++;
       accumulate[key].total += ms;
-      accumulate[key].subtotal += call.sub;
 
-      if (level == 0)
-         console.log(); // empty line
+      // print/write stuff
+      if (_p.cb) {
+         console.log(_p.name.yellow + "() (callback)".green, ("" + ms), "ms".grey);
+      } else {
+         console.log(_p.name.yellow + "() ".green, ("" + ms), "ms".grey);
+      }
    },
    getFilename: function(filename) {
-      return filename.substring(__dirname.length + outputFolder.length);
-   },
-   getPrefix: function(level) {
-      var x = "";
-      for (var i = 0; i < level; i++)
-         x += "   ";
-      return x;
+      return filename.substring(__dirname.length + "/lib-profile".length);
    }
 };
